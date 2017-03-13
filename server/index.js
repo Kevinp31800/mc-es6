e05634784019829f4bf61fb5bd2a200642dbc804
kevin <kevinpuyol@live.fr>
2017-03-13 16:58:35 +0100

    fonction

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-//to do server
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+// import routes from './public/index.html';
+
+// app.use('/', routes);
+
+app.listen(8080, ()=>{
+
+	console.log('now on port 8080')	
+});
--- server/public/js/app.js
+++ server/public/js/app.js
@@ -1,5 +1,26 @@
 (() => {
-  const app = {
-    //todo
-  }
+	const app = {
+		tasks:null,
+		init(){
+			this.getTasks();
+		},
+		getTasks(){
+			fetch('/database/tasks.json')
+			.then(res=>res.json())
+			.then(data=>{
+				this.tasks = data.tasks;
+				this.addTasks();
+			})
+		},
+		addTasks(){
+			// const newli = document.createElement("li");
+			// newli.innerHTML = this.tasks[0].content;
+			// const list = document.getElementById('newTodo');
+			// list.appendChild(newli);
+			this.tasks.map(task=>{
+				document.getElementById('list').innerHTML += `<li>${task.content}</li>`
+			})
+		}
+	}
+	app.init();
 })();
